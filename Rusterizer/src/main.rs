bb9c5c2e90ba27e890dd8182a71904a4d9480bff
Flan <flannyha@gmail.com>
2023-01-26 11:38:30 +0100

    Added minifb and create black window

--- Rusterizer/src/main.rs
+++ Rusterizer/src/main.rs
@@ -1,3 +1,23 @@
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut window =
+        Window::new("a", WIDTH, HEIGHT, WindowOptions::default()).unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16666)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0;
+        }
+
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
